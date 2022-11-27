9b427c95966e882df492b0396b089de6942cd604
ghbcs <ms.fa.rah@hotmail.com>
2022-11-27 05:15:23 -0600

    Create new application API.

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    modelBaseDirectory: 'app_server/models', // model directory name
+    modelBaseDirectory: 'app_api/models', // model directory name
     models: ['*.js', '!db.js'], // model matcher
     data: 'data', // data directory name
     db: 'mongodb://localhost:27017/travlr' // db connection url
